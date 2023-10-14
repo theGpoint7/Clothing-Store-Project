e21b2e90b7b4cfef4538e8552b45b4ca9916ea38
theGpoint7 <griffioen.jesse@gmail.com>
2023-10-14 17:37:03 +0200

    Barebone project in Spring Boot

---
+++ src/test/java/nl/novi/ClothingStore/ClothingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.ClothingStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
